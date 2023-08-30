2c1a385d7699f0315ed34c0361def34c185e9503
türkan şensoy <trksnsy01@gmail.com>
2023-08-30 10:15:24 +0300

    Add project files.

---
+++ src/External/Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Abstraction
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public abstract class ApiController: ControllerBase
+    {
+    }
+}
---
+++ src/External/Presentation/Controller/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controller
+{
+    public sealed class TestController:ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("İşlem Başarılı");
+        }
+    }
+}
